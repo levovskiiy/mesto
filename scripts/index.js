9b8346660f4437d76cb74ec87d2477f4aef434e6
lev cherepanov <levovskiiy@yandex.ru>
2022-06-09 16:30:58 +0500

    ref: rename constants, fix bugs

---
+++ scripts/index.js
@@ -0,0 +1,165 @@
+const profile = document.querySelector('.profile');
+const profileName = profile.querySelector('.profile__name');
+const profileDescription = profile.querySelector('.profile__description');
+const editProfileButton = profile.querySelector('.profile__button-edit');
+const newCardButton = profile.querySelector('.profile__button-add');
+
+const closingButtonList = document.querySelectorAll('.popup__button-close');
+const cardsContainer = document.querySelector('.card-list');
+
+const profilePopup = document.querySelector('.popup_type_edit');
+const newPlacePopup = document.querySelector('.popup_type_add-card');
+const zoomPhotoPopup = document.querySelector('.popup_type_open-photo');
+const imagePlace = zoomPhotoPopup.querySelector('.popup__image');
+const caption = zoomPhotoPopup.querySelector('.popup__image-caption');
+
+const forms = document.forms;
+
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
+];
+
+const escapeClosePopup = (evt) => {
+  const currentPopup = document.querySelector('.popup_opened');
+  if (evt.key === 'Escape') {
+    closePopup(currentPopup);
+  }
+}
+
+const overlayClosePopup = (evt) => {
+  if (evt.target === evt.currentTarget) {
+    closePopup(evt.currentTarget);
+  }
+}
+
+const openPopup = popup => {
+  document.addEventListener('keydown', escapeClosePopup);
+  popup.addEventListener('click', overlayClosePopup);
+
+  popup.classList.add('popup_opened');
+};
+const closePopup = popup => {
+  popup.classList.remove('popup_opened');
+
+  document.removeEventListener('keydown', escapeClosePopup);
+  popup.removeEventListener('click', overlayClosePopup);
+};
+
+/**
+ * Функция вешает слушатели событий на кнопки лайка, удаления, открытия картинки.
+ * @param {Element} like
+ * @param {Element} trash
+ * @param {Element} image
+ */
+const setListeners = (like, trash, image) => {
+  like.addEventListener('click', () => like.classList.toggle('card-list__button-like_state_active'));
+  trash.addEventListener('click', () => trash.closest('.card-list__item').remove());
+  image.addEventListener('click', () => {
+    openPopup(zoomPhotoPopup);
+    imagePlace.src = image.src;
+    imagePlace.alt = image.alt;
+    caption.textContent = image.alt;
+  });
+};
+
+const setAttributes = (element, attrs = {}) => {
+  for (const key in attrs) {
+    element[key] = attrs[key];
+  }
+};
+const getTemplateContent = template => document.querySelector(template).content.cloneNode(true);
+
+/**
+ * Функция создает экземпляр карточки, навешывает слушатели событий.
+ * @param {Object} card
+ * @returns {Element}
+ */
+const createCard = card => {
+  const cardElement = getTemplateContent('#card');
+  const cardImage = cardElement.querySelector('.card-list__image');
+  const place = cardElement.querySelector('.card-list__place');
+  const cardButton = cardElement.querySelector('.card-list__button-like');
+  const cardTrash = cardElement.querySelector('.card-list__trash');
+
+  place.textContent = card.name;
+  setAttributes(cardImage, { src: card.link, alt: card.name });
+  setListeners(cardButton, cardTrash, cardImage);
+
+  return cardElement;
+};
+
+/**
+ * Функция добавляет 1 карточку в начало списка.
+ * @param {object} card
+ * @returns
+ */
+const addCard = card => cardsContainer.prepend(createCard(card));
+
+/**
+ * Добавляет начальные карточки из массива.
+ */
+const initCards = () => {
+  initialCards.forEach(card => cardsContainer.append(createCard(card)));
+};
+
+const insertValues = (/** @type {string} */ name, /** @type {string} */ description) => {
+  forms.editProfile.username.value = name;
+  forms.editProfile.description.value = description;
+};
+
+forms.editProfile.addEventListener('submit', evt => {
+  evt.preventDefault();
+  profileName.textContent = forms.editProfile.username.value;
+  profileDescription.textContent = forms.editProfile.description.value;
+
+  closePopup(profilePopup);
+
+})
+
+forms.newPlace.addEventListener('submit', evt => {
+  evt.preventDefault();
+  addCard({
+    name: forms.newPlace.name.value,
+    link: forms.newPlace.link.value,
+  });
+  closePopup(newPlacePopup);
+})
+
+newCardButton.addEventListener('click', () => openPopup(newPlacePopup));
+
+editProfileButton.addEventListener('click', () => {
+  openPopup(profilePopup);
+  insertValues(profileName.textContent, profileDescription.textContent);
+  forms.editProfile.username.value = profileName.textContent;
+  forms.editProfile.description.value = profileDescription.textContent;
+});
+
+closingButtonList.forEach(closingButton => {
+  closingButton.addEventListener('click', () => closePopup(closingButton.closest('.popup')));
+});
+
+initCards();
