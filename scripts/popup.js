fda0bd046a509ba4a4bc7c9253bec1f0f82e9534
lev cherepanov <levovskiiy@yandex.ru>
2022-06-21 16:28:10 +0500

    ref: убрал очистку форм при закрытии ли открытии Popup

--- scripts/popup.js
+++ scripts/popup.js
@@ -23,9 +23,6 @@ const overlayClosePopup = evt => {
  * @param {HTMLElement} popup
  */
 const openPopup = popup => {
-  if (!popup.classList.contains('popup_type_open-photo')) {
-    popup.querySelector('.popup__form').reset();
-  }
   document.addEventListener('keydown', escapeClosePopup);
   popup.addEventListener('click', overlayClosePopup);
   popup.classList.add('popup_opened');
@@ -35,9 +32,6 @@ const openPopup = popup => {
  * @param {HTMLElement} popup
  */
 const closePopup = popup => {
-  if (!popup.classList.contains('popup_type_open-photo')) {
-    popup.querySelector('.popup__form').reset();
-  }
   popup.classList.remove('popup_opened');
   document.removeEventListener('keydown', escapeClosePopup);
   popup.removeEventListener('click', overlayClosePopup);
